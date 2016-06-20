e6cff7230c2b099ea7e8c6ae73132e9bd26190c5
Xmasjos <xmasjos@hotmail.com>
2016-06-21 01:57:29 +0200

    [Add] Data to Pdb & records & added .toJson

--- app.js
+++ app.js
@@ -8,17 +8,16 @@ if (process.argv.length < 2) {
 }
 
 var fileName = process.argv[2];
+var outputFileName = process.argv[3];
 
 function writeToFile(data) {
-	var jsonData = JSON.stringify(jsonData, null, 2);
+	var jsonData = JSON.stringify(data, null, 2);
 	
-	var outputFileName = './test-output.json';
-	
-	fs.writeFile(outputFilename, jsonData, function (err) {
+	fs.writeFile(outputFileName, jsonData, function (err) {
 		if (err) {
 			console.log(err);
 		} else {
-			console.log("JSON saved to " + outputFilename);
+			console.log("JSON saved to " + outputFileName);
 		}
 	}); 
 }
@@ -34,7 +33,7 @@ ebookParser.parse(fileName, (err, file) => {
 	fileJson.records = [];
 	//TODO: Add bytes for all first item stuff
 	
-	console.log('Read file "' + fileName + '" (' + file.formatName + ')');
+	console.log('Read file "' + fileName + '" (' + file.format.name + ')');
 	if (file.version) {
 		console.log('\tv' + file.version);
 	}
@@ -54,46 +53,28 @@ ebookParser.parse(fileName, (err, file) => {
 	if (file.recordInfos.length > 0) {
 		var recordInfo = file.recordInfos[0];
 		var secondRecordInfo = file.recordInfos[1];
-		//var thirdRecordInfo = file.recordInfos[2];
-		
-		console.log('Info', recordInfo.id, recordInfo.offset, secondRecordInfo.offset - 1);
-		console.log('Attributes: ' + recordInfo.attributes);
-		
-		var parseHelper = require('./lib/parsers/parseHelper');
 		
 		var recordBuffer = file.getRecordBuffer(0);
 		
 		var bytes = [];
 		
-		for (var value in recordBuffer.values()) {
-			bytes.push(value);
+		for (var bufferIx = 0; bufferIx < recordBuffer.length; bufferIx++) {
+			bytes.push(recordBuffer[bufferIx]);
 		}
+		//for (var value in recordBuffer.values()) {
+		//	bytes.push(value);
+		//}
+		
+		fileJson.records.push({});
 		
 		fileJson.records[0].bytes = bytes;
-		
-		//var partStringBytes = parseHelper.buffer_getBytes(file.buffer, recordInfo.offset, recordInfo.offset + 500);
-		//var partString = file.buffer.toString('binary', recordInfo.offset, recordInfo.offset + 500);
-		
-		//if (partStringBytes) {
-		//	console.log(partStringBytes);
-		//} else {
-		//	console.log('No data');
-		//}
-		
-		//parseHelper.buffer_logAllParses(file.buffer, recordInfo.offset, recordInfo.offset + 500);
-		//console.log()
-		
-		//for (var i = recordInfo.offset; i < secondRecordInfo.offset - 1; i += 50) {
-		//	var blaat = file.buffer.toString('ascii', i, i + 50);
-		//	
-		//	console.log(blaat);
-		//}
-		
-		//console.log('Info', secondRecordInfo.id, secondRecordInfo.offset, thirdRecordInfo.offset - 1);
-		//console.log('Attributes: ' + secondRecordInfo.attributes);
 	}
 	
 	console.log();
 	
-	writeFile(fileJson);
+	if (outputFileName) {
+		writeToFile(fileJson);
+	} else {
+		console.log(fileJson);
+	}
 });
